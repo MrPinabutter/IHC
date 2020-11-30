e22b89e78e089066b197cc99518ae1a19bbec3ce
MrPinabutter <vitorafael.r.c@gmail.com>
2020-11-29 21:27:06 -0300

    hold-and-drag

--- index.js
+++ index.js
@@ -1,26 +1,48 @@
 const position = { x: 0, y: 0 }
 
 interact('.draggable').draggable({
-listeners: {
-    start (event) {
-        console.log(`Inicio: x = ${position.x} y = ${position.y}`);
-    },
-    move (event) {
-        position.x += event.dx
-        position.y += event.dy
-
-        event.target.style.transform =
-            `translate(${position.x}px, ${position.y}px)`
+    listeners: {
+        start (event) {
+            console.log(`Inicio: x = ${position.x} y = ${position.y}`);
         },
-    end (event){
-        console.log(`Final: x = ${position.x} y = ${position.y}`);
-    }
-}})
-
+        move (event) {
+            position.x += event.dx
+            position.y += event.dy
+            
+            event.target.style.transform =
+            `translate(${position.x}px, ${position.y}px)`
+        }
+    }})
+    
 interact('.event').on('tap', (event) => {
     console.log(event);
     event.target.style.backgroundColor = `rgb(${getRandomInt(0, 255)}, ${getRandomInt(0, 255)}, ${getRandomInt(0, 255)})`
 })
+    
+const position1 = { x: 0, y: 0 }
+
+interact('.hold').on('hold', (event) => {
+    console.log("segurou");
+    event.currentTarget.classList.toggle('drig')
+})
+
+interact('.drig').draggable({
+    listeners: {
+        start (event) {
+            console.log(`Inicio: x = ${position1.x} y = ${position1.y}`);
+        },
+        move (event) {
+            position1.x += event.dx
+            position1.y += event.dy
+            
+            event.target.style.transform =
+            `translate(${position1.x}px, ${position1.y}px)`
+        },
+        end (event){
+            event.currentTarget.classList.remove('drig')
+        }
+    }})
+
 
 var angle = 0
 
